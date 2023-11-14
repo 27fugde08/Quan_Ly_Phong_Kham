599c4fb3e3c8e30366a3d0eff37c0065a324f600
27fudge08 <nguyentrongduy27082002@gmail.com>
2023-11-14 14:55:49 +0700

    done 91

--- src/route/web.js
+++ src/route/web.js
@@ -55,6 +55,8 @@ let initWebRoutes = (app) => {
 
 
     router.post('/api/create-new-specialty', specialtyController.createSpecialty);
+    router.get('/api/get-specialty', specialtyController.getAllSpecialty);
+
     return app.use("/", router);
 };
 
