67af6611b3c74ba15efa49c6e103fa759f73421b
27fudge08 <nguyentrongduy27082002@gmail.com>
2023-11-15 02:06:19 +0700

    done 97

--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -28,84 +28,75 @@ let createClinic = (data) => {
         }
     })
 }
-// let getAllSpecialty = () => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-//             let data = await db.Specialty.findAll({
+let getAllClinic = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Clinic.findAll({
 
-//             });
-//             if (data && data.length > 0) {
-//                 data.map(item => {
-//                     item.image = new Buffer(item.image, 'base64').toString('binary')
-//                     return item;
-//                 })
-//             }
-//             resolve({
-//                 errCode: 0,
-//                 errMessage: 'Okay',
-//                 data
-//             })
-//         } catch (e) {
-//             reject(e)
-//         }
-//     })
-// }
+            });
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.image = new Buffer(item.image, 'base64').toString('binary')
+                    return item;
+                })
+            }
+            resolve({
+                errCode: 0,
+                errMessage: 'Okay',
+                data
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 
-// let getDetailSpecialtyById = (inputId, location) => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-//             if (!inputId || !location) {
-//                 resolve({
-//                     errCode: 1,
-//                     errMessage: 'Missing parameter'
-//                 })
-//             }
-//             else {
-//                 let data = await db.Specialty.findOne({
-//                     where: {
-//                         id: inputId
-//                     },
-//                     attributes: ['descriptionHTML', 'descriptionMarkdown'],
-//                 })
-//                 if (data) {
-//                     let doctorSpecialty = [];
-//                     if (location === 'ALL') {
-//                         doctorSpecialty = await db.Doctor_Infor.findAll({
-//                             where: { specialtyId: inputId },
-//                             attributes: ['doctorId', 'provinceId'],
-//                         })
-//                     } else {
-//                         //find by location
-//                         doctorSpecialty = await db.Doctor_Infor.findAll({
-//                             where: {
-//                                 specialtyId: inputId,
-//                                 provinceId: location
-//                             },
-//                             attributes: ['doctorId', 'provinceId'],
-//                         })
-//                     }
-//                     data.doctorSpecialty = doctorSpecialty;
-//                 } else data = {}
-//                 resolve({
-//                     errMessage: "OK",
-//                     errCode: 0,
-//                     data
-//                 })
-//             }
+let getDetailClinicById = (inputId, location) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            }
+            else {
+                let data = await db.Clinic.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown'],
+                })
+                if (data) {
+                    let doctorClinic = [];
 
-//             resolve({
-//                 errCode: 0,
-//                 errMessage: 'Okay',
-//                 data
-//             })
-//         } catch (e) {
-//             reject(e)
-//         }
-//     })
-// }
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: { clinicId: inputId },
+                        attributes: ['doctorId'],
+                    })
+
+                    data.doctorClinic = doctorClinic;
+                } else data = {}
+                resolve({
+                    errMessage: "OK",
+                    errCode: 0,
+                    data
+                })
+            }
+
+            resolve({
+                errCode: 0,
+                errMessage: 'Okay',
+                data
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 
 module.exports = {
     createClinic: createClinic,
-    // getAllSpecialty: getAllSpecialty,
-    // getDetailSpecialtyById: getDetailSpecialtyById
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
 }
